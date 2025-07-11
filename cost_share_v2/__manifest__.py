5333f3b2fa6ece5b60770eb80b6ab693ad798652
Shaf3i1code <mohannad.elshafei@gmail.com>
2025-07-11 10:13:11 +0300

    Add files via upload

---
+++ cost_share_v2/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "cost_sharing_v2",
+    'version': '18.0.0.1',
+    'summary': """
+        Make Cost Share Fixed Again""",
+
+    'description': """
+        By Editing mrp and stock move
+    """,
+
+    'author': "Mohanad Elshafei",
+    'company': "El Welely Group",
+    'website': "https://www.linkedin.com/in/mohandelshafei/",
+    'category': 'Manufacturing',
+    'license': 'LGPL-3',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'mrp', 'mrp_account_enterprise', 'purchase_mrp' ,'documents', 'mrp_plm'],
+
+    # always loaded
+    'data': [
+        # 'report/mrp_cost_structure_report.xml',
+        # 'security/ir.model.access.csv',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+}
---
+++ cost_share_v2/report/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import mrp_report_bom_structure
+from . import mrp_cost_structure
+from . import mrp_report
+from . import mrp_report_mo_overview
