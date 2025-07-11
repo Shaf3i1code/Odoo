3c89d188ca1c02a4a4a61b70ead8cae571e4f566
Shaf3i1code <mohannad.elshafei@gmail.com>
2025-07-11 10:41:27 +0300

    Update __manifest__.py

--- cost_share_v2/__manifest__.py
+++ cost_share_v2/__manifest__.py
@@ -14,7 +14,7 @@
     'website': "https://www.linkedin.com/in/mohandelshafei/",
     'category': 'Manufacturing',
     'license': 'LGPL-3',
-    'price': 10,000,
+    'price': 10000,
     'currency': 'EGP',
 
     # any module necessary for this one to work correctly
