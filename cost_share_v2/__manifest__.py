b26b3e66c48c11d3b8179413e9ebe38d2d2567b0
Shaf3i1code <mohannad.elshafei@gmail.com>
2025-07-11 10:45:03 +0300

    Update __manifest__.py

--- cost_share_v2/__manifest__.py
+++ cost_share_v2/__manifest__.py
@@ -16,7 +16,7 @@
     'license': 'LGPL-3',
     'price': 200,
     'currency': 'USD',
-    'images': ['cost_share_v2\static\description'],
+    'images': ['static/description/INFOTIX.png'],
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mrp', 'mrp_account_enterprise', 'purchase_mrp' ,'documents', 'mrp_plm'],
