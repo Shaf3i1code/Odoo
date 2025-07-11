c9cb87d2f2c86b18f2dff8ad7f315dc7c9bd1f3b
Shaf3i1code <mohannad.elshafei@gmail.com>
2025-07-11 10:40:19 +0300

    Update __manifest__.py

--- cost_share_v2/__manifest__.py
+++ cost_share_v2/__manifest__.py
@@ -14,6 +14,8 @@
     'website': "https://www.linkedin.com/in/mohandelshafei/",
     'category': 'Manufacturing',
     'license': 'LGPL-3',
+    'price': 10,000,
+    'currency': 'EGP',
 
     # any module necessary for this one to work correctly
     'depends': ['base', 'mrp', 'mrp_account_enterprise', 'purchase_mrp' ,'documents', 'mrp_plm'],
