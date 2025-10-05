6f0efaa5ac72d8a18d1cb2b2da4ae1b6cac2045d
Shaf3i1code <mohannad.elshafei@gmail.com>
2025-10-05 18:34:10 +0300

    Update __manifest__.py

--- cost share/__manifest__.py
+++ cost share/__manifest__.py
@@ -31,7 +31,7 @@ Ideal for:
     'website': "https://www.linkedin.com/in/mohandelshafei/",
     'category': 'Manufacturing',
     'license': 'LGPL-3',
-    'price': 82,
+    'price': 300,
     'currency': 'USD',
     'images': ['static/description/icon.png','static/description/cover.jpg','static/description/screenshot1.png','static/description/screenshot2.png'],
 
